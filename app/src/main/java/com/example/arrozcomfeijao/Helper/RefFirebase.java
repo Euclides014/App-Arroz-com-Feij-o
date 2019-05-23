b2e853a41eeef77b46a666b996f2aa5375c202fb
Mateus de Sousa <mateusdevsousa@gmail.com>
2019-05-23 02:01:11 -0300

    Criação do arquivo de referencias do Firebase

---
+++ app/src/main/java/com/example/arrozcomfeijao/Helper/RefFirebase.java
@@ -0,0 +1,57 @@
+package com.example.arrozcomfeijao.Helper;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class RefFirebase {
+    private static FirebaseAuth referenceAuth;
+    private static FirebaseFirestore referenceStore;
+    private static StorageReference referenceStorage;
+
+    public static FirebaseFirestore getFirebaseStoreCliente(){
+        if (referenceStore == null){
+            referenceStore = FirebaseFirestore.getInstance();
+        }
+        return referenceStore;
+    }
+
+    public static FirebaseFirestore getFirebaseStoreGarcon(){
+        if (referenceStore == null){
+            referenceStore = FirebaseFirestore.getInstance();
+        }
+        return referenceStore;
+    }
+
+    public static FirebaseFirestore getFirebaseStoreCozinheiro(){
+        if (referenceStore == null){
+            referenceStore = FirebaseFirestore.getInstance();
+        }
+        return referenceStore;
+    }
+
+    public static FirebaseFirestore getFirebaseStoreGerente(){
+        if (referenceStore == null){
+            referenceStore = FirebaseFirestore.getInstance();
+        }
+        return referenceStore;
+    }
+
+    public static FirebaseAuth getFirebaseAuth(){
+        if (referenceAuth == null){
+            referenceAuth = FirebaseAuth.getInstance();
+        }
+        return referenceAuth;
+    }
+
+
+    public static StorageReference getFirebaseStorege(){
+        if (referenceStorage == null){
+            referenceStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return referenceStorage;
+    }
+}
+
